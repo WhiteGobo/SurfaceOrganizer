92767ac31b459752e98dfdd21c93a76e7fbbb57a
WhiteGobo <asd@notavailable.xxx>
2021-05-12 12:04:50 +0200

    filepath from testdirectory testfile

--- test/test.py
+++ test/test.py
@@ -12,13 +12,25 @@ import unittest
 import importlib.resources
 import bpy
 import my_io_mesh_ply as main
+from my_io_mesh_ply import plyhandler
+
+import logging
+logger = logging.getLogger( __name__ )
 
 class test_blender_plyimporter( unittest.TestCase ):
     def setUp( self ):
+        logging.basicConfig( level=logging.DEBUG )
         main.register()
+
+
     def test_load_and_save_ascii( self ):
-        #with importlib.resources.path()
+        import my_io_mesh_ply.plyhandler.get_surfacemap_from_ply.tests \
+                    as testdirectory1
+        override = {}
+        with importlib.resources.path( testdirectory1, "tmp.ply" ) as filepath:
+            logger.critical( filepath )
         pass
+
 
     def tearDown( self ):
         main.unregister()
